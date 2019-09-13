db5239f98d3272e26830189ed1837814402cd471
Dominic <dominic.solomon.2022@student.lhschools.org>
2019-09-13 09:59:00 -0400

    Added math operations

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-int firstnumber,secondnumber;
+        // write your code here
+        int firstnumber,secondnumber;
 
-firstnumber= 5;
-secondnumber= 10;
+        firstnumber= 5;
+        secondnumber= 10;
+
+        System.out.print(firstnumber);
+        System.out.print("firstnumber + secondnumber is"+ (firstnumber+secondnumber) );
+        System.out.print("firstnumber - secondnumber is"+ (firstnumber-secondnumber) );
+        System.out.print("firstnumber / secondnumber is"+ (firstnumber/secondnumber) );
+        System.out.print("firstnumber * secondnumber is"+ (firstnumber*secondnumber) );
+        System.out.print("firstnumber * secondnumber is"+ (firstnumber*secondnumber) );
+
 
 
 
