619e078068ee64494866bc6ed0faa6dfba5832c3
Dominic <dominic.solomon.2022@student.lhschools.org>
2019-09-17 09:48:15 -0400

    Added  variables dog and house and added there equations

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,14 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         int firstnumber,secondnumber;
-
+        String firstWord ;
+        Sring secondWord ;
+firstWord= dog;
+secondWord= house;
         firstnumber= 5;
         secondnumber= 10;
-
+        System.out.print(firstWord+secondword)
+        System.out.print(firstword+firstnumber)
 //        System.out.print("firstnumber + secondnumber is"+ (firstnumber+secondnumber) );
 //        System.out.print("firstnumber - secondnumber is"+ (firstnumber-secondnumber) );
 //        System.out.print("firstnumber / secondnumber is"+ (firstnumber/secondnumber) );
